506c064204b9f519aaedcbfd6da0b5cec8ce900e
NoMap7 <sumansahu417@hotmail.com>
2021-11-30 09:37:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api/index'
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+            setCoordinates({lat: latitude, lng: longitude});
+        });
+    }, []);
+
+    useEffect(() => {
+        getPlacesData(bounds.sw, bounds.ne)
+            .then((data) => {
+                console.log(data);
+                setPlaces(data);
+            })
+    }, [coordinates, bounds]);
+
+    return (
+        <>
+        <CssBaseline />
+        <Header onPlaceChanged={onPlaceChanged} onLoad={onLoad} />
+        <Grid container spacing={3} style={{ width: '100%' }}>
+          <Grid item xs={12} md={4}>
+            <List
+              isLoading={isLoading}
+              childClicked={childClicked}
+              places={filteredPlaces.length ? filteredPlaces : places}
+              type={type}
+              setType={setType}
+              rating={rating}
+              setRating={setRating}
+            />
+          </Grid>
+          <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Map
+              setChildClicked={setChildClicked}
+              setBounds={setBounds}
+              setCoords={setCoords}
+              coords={coords}
+              places={filteredPlaces.length ? filteredPlaces : places}
+              weatherData={weatherData}
+            />
+          </Grid>
+        </Grid>
+      </>
+    );
+}
+
+export default App;
